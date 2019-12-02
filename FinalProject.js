8b01683884eae96cde036ef681b6d3c6394a7a15
CooperRhoads <srhoads22@nscds.org>
2019-12-02 17:19:37 +0000

    there were no changes made

---
+++ FinalProject.js
@@ -0,0 +1,9 @@
+var diameter = 400 ; 
+
+/*TweenMax.to("#electron1",0.5,{rotation:-90,delay:0.5, ease:Sine.easeOut});
+bezier:{curviness: 3.5,values:[{x:520,y:-300},{x:-diameter/40,y:diameter/20},{x:520,y:diameter/20},{x:diameter/40,y:diameter/20},{x:520,y:-300}]}
+,ease:Power9.easeNone,repeat:5});*/
+
+TweenMax.to("#electron2", 5 ,{
+bezier:{curviness:3.0,values:[{x:520,y:-300},{x:-diameter/40,y:diameter/20},{x:520,y:diameter/20},{x:diameter/40,y:diameter/20},{x:520,y:520}]}
+,ease:Power0.easeNone,repeat:-1});
