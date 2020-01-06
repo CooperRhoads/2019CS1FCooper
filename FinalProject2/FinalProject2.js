4913277f3b4397b52a4988ee97eb82a34ddb43a9
CooperRhoads <srhoads22@nscds.org>
2020-01-06 17:48:34 +0000

    added to second project code.

---
+++ FinalProject2/FinalProject2.js
@@ -0,0 +1,61 @@
+var diameter = 228 ; 
+
+TweenMax.to("#electron1", 5 ,{
+bezier:{curviness:1.5,values:[
+    
+    {x:0,y:0},
+    {x:diameter/2,y:diameter/2},
+    {x:0,y:diameter},
+    {x:-diameter/2,y:diameter/2},
+    {x:0,y:0}
+
+]}
+,ease:Power0.easeNone,repeat:-1});
+
+
+
+
+
+TweenMax.to("#electron2", 5 ,{
+bezier:{curviness:1.5,values:[
+    
+    {x:0,y:0},
+    {x:-diameter/2,y:-diameter/2},
+    {x:0,y:-diameter},
+    {x:diameter/2,y:-diameter/2},
+    {x:0,y:0}
+
+]}
+,ease:Power0.easeNone,repeat:-1});
+
+
+
+
+
+TweenMax.to("#electron2", 5 ,{
+bezier:{curviness:1.5,values:[
+    
+    {x:-diameter/2,y:0},
+    {x:diameter/2,y:diameter/2},
+    {x:0,y:-diameter},
+    {x:-diameter/2,y:diameter/2},
+    {x:0,y:0}
+
+]}
+,ease:Power0.easeNone,repeat:-1});
+
+
+
+
+
+TweenMax.to("#electron2", 5 ,{
+bezier:{curviness:1.5,values:[
+    
+    {x:0,y:0},
+    {x:-diameter/2,y:diameter/2},
+    {x:0,y:-diameter},
+    {x:diameter/2,y:-diameter/2},
+    {x:0,y:0}
+
+]}
+,ease:Power0.easeNone,repeat:-1});
